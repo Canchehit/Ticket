f2b42e679b15518490697ea4852b70aae3ddf09f
Canchehit <luisalejandroberreondoflorian@gmail.com>
2025-03-19 17:03:11 -0600

    Add files via upload

---
+++ SistemaTickets.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistematickets;
+
+/**
+ *
+ * @author luisa
+ */
+public class SistemaTickets {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
